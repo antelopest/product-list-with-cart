cf4983544ad64e79302f7169039f9914f974cec2
antelopest <antelopest@gmail.com>
2025-07-22 15:11:17 +0700

    main change main order

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
 import './styles/main.scss';
 
-import { OrderModule } from './modules/order/order.module.js';
-
-OrderModule.register();
+import '@orderModule/order.module.js';
--- src/modules/order/order.module.js
+++ src/modules/order/order.module.js
@@ -2,18 +2,10 @@ import Module from '@module/Module.js';
 
 import { AppDIContainer } from '@appDIContainer';
 
-// import ProductsService from './services/products/products.service.js';
-import CartService from './services/cart/cart.service.js';
-
-// import ProductCardComponent from './components/product-card/product-card.component.js';
-// import CartComponent from './components/cart/cart.component.js';
-// import ProductsComponent from './components/products/products.component.js';
-// import SvgLoaderComponent from '@orderModule/components/svg-loader/svg-loader.component.js';
-
 export const OrderModule = new Module({
   services: [
     // [ProductsService.serviceName, ProductsService, ProductsService.isSingleton],
-    [CartService.serviceName, CartService, CartService.isSingleton]
+    // [CartService.serviceName, CartService, CartService.isSingleton]
   ],
   components: [
     // [SvgLoaderComponent.tag, SvgLoaderComponent],
@@ -23,3 +15,5 @@ export const OrderModule = new Module({
   ],
   AppDIContainer
 });
+
+OrderModule.register();
